8b365ad9e55acfdc1b373a6e32cefbb2d280a1b6
Tarun Dadlani <tarun3dadlani@gmail.com>
2020-08-12 19:28:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Chatbot from "react-chatbot-kit";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import config from "./BotConfig/config";
+import MessageParser from "./Components/MessageParser";
+import ActionProvider from "./Components/ActionProvider";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Chatbot
+          actionProvider={ActionProvider}
+          messageParser={MessageParser}
+          config={config}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
